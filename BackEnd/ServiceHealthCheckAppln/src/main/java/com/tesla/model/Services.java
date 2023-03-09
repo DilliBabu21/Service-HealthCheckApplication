30d05aff8f986e026bc310195b424ff572985197
DilliBabu21 <98866542+DilliBabu21@users.noreply.github.com>
2023-03-09 15:56:14 +0530

    Added Custom Header

--- BackEnd/ServiceHealthCheckAppln/src/main/java/com/tesla/model/Services.java
+++ BackEnd/ServiceHealthCheckAppln/src/main/java/com/tesla/model/Services.java
@@ -1,14 +1,19 @@
 package com.tesla.model;
 
 import java.sql.Timestamp;
+import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -49,6 +54,8 @@ public class Services {
 	@Column(nullable = false)
 	private Timestamp checkedAt = new Timestamp(System.currentTimeMillis());
 
-
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	@JoinColumn(name = "service_id")
+	private List<Header> customHeaders;
 
 }
